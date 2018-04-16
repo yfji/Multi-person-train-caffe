a375932465941e964bc0c293b7acc6ac6e4bd50b
yfji <jyf131462@163.com>
2018-04-16 21:22:40 +0800

    fuck TransformJoints

--- cpm_data_transformer.cpp
+++ cpm_data_transformer.cpp
@@ -260,8 +260,8 @@ void CPMDataTransformer<Dtype>::TransformJoints(Joints& j) {
     }
   }
   else if(np==24){
-    int FASHION_to_ours_1[24]={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,24};
-    int FASHION_to_ours_2[24]={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,24};
+    int FASHION_to_ours_1[24]={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24};
+    int FASHION_to_ours_2[24]={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24};
     jo.joints.resize(np);
     jo.isVisible.resize(np);
     for(int i=0;i<np_ours;i++){
